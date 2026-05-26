db3b7a892a13f2d807d43f055919d144fb0b734a
Mahadeb Bera <mbera2175@gmail.com>
2026-05-26 19:34:33 +0530

    Update driver_socket_service.dart

--- lib/services/driver_socket_service.dart
+++ lib/services/driver_socket_service.dart
@@ -17,7 +17,7 @@ class DriverSocketService {
       disconnect();
 
       final wsUrl =
-          'ws://13.232.171.208/ws/driver/$driverId';
+    'ws://13.232.171.208:8000/ws/driver/$driverId';
 
       _channel = WebSocketChannel.connect(
         Uri.parse(wsUrl),
